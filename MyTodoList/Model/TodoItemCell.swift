b8de62a91faee4c895c09a36ed6214b08ab0a96f
tai chen <tai@brixtondynamics.co.uk>
2018-10-08 12:54:22 +0200

    initial commit index and list tableviews

---
+++ MyTodoList/Model/TodoItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoCell.swift
+//  MyTodoList
+//
+//  Created by tai chen on 08/10/2018.
+//  Copyright © 2018 TPBSoftware. All rights reserved.
+//
+
+import UIKit
+
+class TodoItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyTodoList/View/IndexViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by tai chen on 08/10/2018.
+//  Copyright © 2018 TPBSoftware. All rights reserved.
+//
+
+import UIKit
+
+class IndexViewController: UITableViewController {
+
+    
+    let items = ["Daily", "Monthly", "Yearly"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IndexItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected list \(items[indexPath.row])")
+        performSegue(withIdentifier: "goToList", sender: self)
+    }
+
+}
+
+
+
